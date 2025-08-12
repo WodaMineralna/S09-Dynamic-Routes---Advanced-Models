281241f14f6ae0b99a58d44fb4f1008caed010dc
WodaMineralna <raknieborak65@gmail.com>
2025-08-12 21:26:13 +0200

    feat(admin-controller): implement edit product functionality
    getEditProduct:
      - Renders the product edit form page
      - If 'edit' query parameter does not equal true, redirects to main page
      - If no product ID was defined or it doesn't match any existing product IDs, redirects to main page
    
    postEditProduct:
      - Receives productId and updated fields from request body
      - Creates new Product instance with updated fields
      - Call Product.save method with productId to update existing product
      - Redirect to main page after finishing

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,19 +1,47 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
+    editing: false,
   });
 };
 
-exports.postAddProduct = (req, res, next) => {
+exports.postAddProduct = async (req, res, next) => {
   const { title, imageUrl, description, price } = req.body;
   const product = new Product(title, imageUrl, description, price);
-  product.save();
+  await product.save();
+  res.redirect("/");
+};
+
+exports.getEditProduct = async (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+
+  const prodId = req.params.productId;
+  const product = await Product.findById(prodId);
+  if (product.id === "undefined") {
+    return res.redirect("/"); // not the best UX, but it's a dummy project
+  }
+
+  res.render("admin/edit-product", {
+    pageTitle: "Edit Product",
+    path: "/admin/edit-product",
+    editing: editMode,
+    product,
+  });
+};
+
+exports.postEditProduct = async (req, res, next) => {
+  const prodId = req.body.productId;
+  const { title, imageUrl, description, price } = req.body;
+  const product = new Product(title, imageUrl, description, price);
+
+  console.log("controllers/admin.js | Edited productId: ", prodId);
+  await product.save(prodId);
   res.redirect("/");
 };
 
